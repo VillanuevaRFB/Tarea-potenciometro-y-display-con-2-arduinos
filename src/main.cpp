e69a3ec17c53d69e00a03a3cdaa206a8edca12f1
VillanuevaRFB <rodrigo.villanuevaa@usil.pe>
2025-05-29 20:56:06 -0500

    leer potenciometro, convertir y mandar por tx

--- src/main.cpp
+++ src/main.cpp
@@ -3,65 +3,37 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-float value=0;
-int flag=0;
-void config_ADC(void){
-  ADCSRA|=(1<<ADEN)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
-  ADMUX|=(1<<REFS0);
-}
+unsigned char voltaje=0;
 
-ISR(ADC_vect){
-  unsigned char low=ADCL;
-  unsigned char high=ADCH;
-  value=(high*256+low)*(50.0/1023.0);
-  UCSR0B|=(1<<UDRIE0);
+void config_ADC(void){
+  ADCSRA|=(1<<ADEN)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);//AVcc, prescaler 128
+  ADMUX|=(1<<REFS0);//canal ADC0
 }
 
 void config_USART(void){
-  UCSR0B|=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
-  UCSR0C|=(1<<UCSZ00)|(1<<UCSZ01);
-  UBRR0=103;//9600 baudrate
+  UBRR0=103;//9600 baudios a 16 MHz
+  UCSR0B|=(1<<TXEN0)|(1<<RXEN0);//habilita tx y rx
+  UCSR0B|=(1<<UDRIE0);//activa interrupcion
+  UCSR0C|=(1<<UCSZ01)|(1<<UCSZ00);
 }
 
-unsigned char dato=0;
+ISR(ADC_vect){
+  unsigned int valor=ADC;
+  voltaje=(valor*50)/1023;//escala a 0–50
+  UCSR0B|=(1<<UDRIE0);//activa envio por TX
+}
+
 ISR(USART_UDRE_vect){
-  switch(flag){
-    case 0:
-    UDR0=(unsigned char)(value/10)%10+'0';
-    flag=1;
-    break;
-    case 1:
-    UDR0=(unsigned char)(46);
-    flag=2;
-    break;
-    case 2:
-    UDR0=((unsigned char)(value))%10+'0';
-    flag=3;
-    break;
-    case 3:
-    UDR0=(unsigned char)(13);
-    flag=4;
-    break;
-    case 4:
-    UDR0=(unsigned char)(10);
-    flag=0;
-    UCSR0B&=~(1<<UDRIE0);
-   _delay_ms(100);
-    break;
-  }
+  UDR0=voltaje;//envia valor a tx
+  UCSR0B&=~(1<<UDRIE0);//desactiva interrupción tras enviar
 }
 
-ISR(USART_RX_vect){
-  dato=UDR0;
-  if(dato=='a'){
-    dato=0;
-    ADCSRA|=(1<<ADSC);
-  }
-}
 int main(void){
-  config_USART();
   config_ADC();
+  config_USART();
   sei();
   while(1){
+    ADCSRA|=(1<<ADSC);//conversion ADC
+    _delay_ms(100);
   }
 }
