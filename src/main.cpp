9af801e6f18eddbeb6b30bcfc3ac86e3781034ec
VillanuevaRFB <rodrigo.villanuevaa@usil.pe>
2025-05-29 21:07:16 -0500

    mostrar valor en displays, tarea terminada

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+/*
 unsigned char voltaje=0;
 
 void config_ADC(void){
@@ -35,5 +36,37 @@ int main(void){
   while(1){
     ADCSRA|=(1<<ADSC);//conversion ADC
     _delay_ms(100);
+  }
+}
+*/
+unsigned char valor=0;//valor recibido
+
+void config_USART(void){
+  UBRR0=103;//9600 baudios
+  UCSR0B|=(1<<RXEN0)|(1<<RXCIE0);//habilita rx y su interrupcion
+  UCSR0C|=(1<<UCSZ01)|(1<<UCSZ00);
+}
+
+ISR(USART_RX_vect){
+  valor=UDR0;//guarda valor recibido
+}
+
+void mostrar_display(unsigned char numero, unsigned char digito){
+  PORTD=(PORTD&0x0F)|(numero<<4);
+  PORTB&=~(1<<digito);//activa displays
+  _delay_ms(5);
+  PORTB|=(1<<digito);//apaga displays
+}
+
+int main(void){
+  DDRD|=0xF0;//PD4–PD7 displays
+  DDRB|=0x03;//PB0 y PB1 multiplexado
+  config_USART();
+  sei();
+  while(1){
+    unsigned char entero=valor/10; //entero del voltaje
+    unsigned char decimal=valor%10; //decimal del voltaje
+    mostrar_display(decimal,0); //muestra decimal en PB0
+    mostrar_display(entero,1); //muestra entero en PB1
   }
 }
